a756ee6661fb5475df7d86fa8f6c29683a427145
User <User@DESKTOP-N20GJMT>
2022-08-23 20:27:01 +0530

    Add project files.

---
+++ TestingPractice/LeapYear.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace TestingPractice
+{
+    public  class LeapYear
+    {
+       
+        
+        public static string CheckLeapYearOrNot(int year)
+        {
+            //output.WriteLine("year"+year);
+            int num = year;// Convert.ToInt32(Console.ReadLine());
+            if (num % 4 == 0)
+            {
+                if (num % 100 == 0)
+                {
+                    if (num % 400 == 0)
+                    {
+                       return num + " is a leap year";
+                    }
+                    else
+                    {
+                        return num + "  is a not an leap year";
+                    }
+                }
+                else
+                {
+                    if (num % 400 == 0)
+                    {
+                        return  num + " is a NA";
+                    }
+                    else
+                    {
+                        return num + " is a leap year";
+                    }
+                }
+            }
+            else
+            {
+                if (num % 100 == 0)
+                {
+                    if (num % 400 == 0)
+                    {
+                        return num + "  is a  NA";
+                    }
+                    else
+                    {
+                        return num + " is a  NA";
+                    }
+                }
+                else
+                {
+                    if (num % 400 == 0)
+                    {
+                        return num + " is a  NA";
+                    }
+                    else
+                    {
+                        return num + "  is a  Not an leap year";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestingPractice/SeleniumBasics1.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit.Abstractions;
+
+namespace TestingPractice
+{
+    public class SeleniumBasics1
+    {
+        private readonly ITestOutputHelper output;
+
+        public SeleniumBasics1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        [Fact]
+        public void LaunchBrowser()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.cricbuzz.com");
+            Console.WriteLine(driver.Title + " " + driver.Url);
+        }
+
+        [Fact]
+        public void testleapyear2000()
+        {
+            output.WriteLine(LeapYear.CheckLeapYearOrNot(2000));
+            output.WriteLine(LeapYear.CheckLeapYearOrNot(1999));
+        }
+
+        [Fact]
+        public void testing2Methods()
+        {
+            Employee Arun = new Employee(15, "Arunachalam", "RM", "07-09-2020", "ASE", "Testing", 20000.89);
+            output.WriteLine(Arun.method1());
+            output.WriteLine(Arun.method2());
+
+        }
+
+
+       
+
+    }
+}
